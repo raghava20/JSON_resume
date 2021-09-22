2b9c78ce77629d674baaa693089c8a9e449026b2
Raghava Anand M <86498250+raghava20@users.noreply.github.com>
2021-09-22 11:40:34 +0530

    JSON resume format

---
+++ resume.js
@@ -0,0 +1,55 @@
+var resume = {
+    name: "Raghava Anand",
+    address:
+      "S/o Muthu rathina pandi S, NO: 14, pallavan street, v.o.c nagar, pammal, chennai - 600075.",
+    email: "raghavaanand22@gmail.com",
+    contactNo: "8015258696",
+    carrierObjective:
+      "Seeking a position that will utilize my creative thinking dedication and efficiency so that along with contributing my best to the organisation, I can continue riding on the learing curve.",
+    educationalQualification: [
+      {
+        course: "B.E",
+        institution: "SNS College of Technology, coimbatore",
+        university: "Autonomous Affiliated Anna University, chennai",
+        yearOfPassing: "2018",
+        percentageOfMark: "89.9"
+      },
+      {
+        course: "HSC",
+        institution: "St.Sebastian Matriculation HSS, chennai",
+        Board: "State Board",
+        yearOfPassing: "2014",
+        percentageOfMark: "92.3"
+      },
+      {
+        course: "SSLC",
+        institution: "St.Sebastian Matriculation HSS, chennai",
+        Board: "State Board",
+        yearOfPassing: "2012",
+        percentageOfMark: "86.6"
+      }
+    ],
+    experience: [
+      {
+        till2021:
+          "Worked as a Specialist Video Ops at Paragon Digital Services Pvt Ltd.",
+        role:
+          "Handled the video Optimization and Upload & Optimization on Youtube. Consequently performed the task like Analytical grouping, Back Optimization, Comment Moderation, Channel Sweeping, Multi tasking, Reporting and Bulk task for the US News Channels on Youtube"
+      },
+      {
+        till2019: "Worked as a Operation Incharge at Esthell Rubbers Pvt Ltd.",
+        role:
+          "Responsiblity of Planning and assigning work for daily production of Tyre recycling and have a tendency to handle maintenance services too."
+      }
+    ],
+    personalDetails: {
+      fatherName: "Muthu rathina pandi S",
+      motherName: "Jaya Johnsi",
+      dateOfBirth: "20/07/97",
+      languagesKnown:
+        "Proficient - Tamil, English (Read, Write, Speak) , Beginner - Hindi (Read, Write)"
+    }
+  };
+  
+  console.log(resume.personalDetails);
+  
